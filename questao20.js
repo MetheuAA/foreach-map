c9e0f26ac78a491a465bfab439832ecbedc15c92
MetheuAA <matheo200817@gmail.com>
2024-04-24 08:39:10 -0400

    add questao20

---
+++ questao20.js
@@ -0,0 +1,6 @@
+/*
+Questão 20 - Qual é a utilidade de forEach() se ele não retornar nenhum valor?
+
+A utilidade do método forEach() está na sua capacidade de iterar sobre os elementos de um array e executar uma ação para cada elemento, como alterar os elementos do array original ou executar alguma lógica baseada nos elementos.
+
+*/
