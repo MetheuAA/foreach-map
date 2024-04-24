a8b48d2a59a806206605eb1493c041e64724f248
MetheuAA <matheo200817@gmail.com>
2024-04-24 08:38:32 -0400

    add questao12

---
+++ questao12.js
@@ -0,0 +1,6 @@
+/*
+Questão 12 - Qual é a diferença entre map() e forEach() em relação ao retorno 
+de valores de uma função de callback?
+
+forEach() executa a a função callback uma vez para cada elemento do array – diferentemente de map() ou reduce() , ele sempre retorna o valor undefined e não é encadeável.
+*/
