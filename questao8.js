b4fe5035343ad0492f4c3b4aa871279ced2b8206
MetheuAA <matheo200817@gmail.com>
2024-04-24 11:07:26 -0400

    modificado questao8

--- questao8.js
+++ questao8.js
@@ -2,3 +2,10 @@
 Questão 08 – Crie um algoritmo usando o método forEach() para calcular a 
 média de todos os números em um array?
 */
+
+const array = [1, 2, 3, 4 ,5];
+let media = 0
+array.forEach(function(elemento) {
+    media += elemento/array.length
+});
+console.log(media);
