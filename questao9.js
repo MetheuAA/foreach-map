7bddff7663fede9a8a5a1db513fa09d4eef1f3ec
MetheuAA <matheo200817@gmail.com>
2024-04-24 08:38:17 -0400

    add questao9

---
+++ questao9.js
@@ -0,0 +1,6 @@
+/*
+Questão 09 - Qual é a diferença fundamental entre map() e forEach()?
+
+MAP: Cria um novo array com a mesma quantidade de elementos
+FOR EACH: Percorre o array os modificando-os
+*/
