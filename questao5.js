ca39fb2e920870d1d311fcf69767f31678fe36bc
MetheuAA <matheo200817@gmail.com>
2024-04-24 08:37:59 -0400

    add questao5

---
+++ questao5.js
@@ -0,0 +1,7 @@
+/*
+Questão 05 – Crie um algoritmo usando o método map() para criar um novo 
+array com o dobro de cada número ímpar em um array original?*/
+
+const array = [1, 3, 5, 7, 9]
+const soma = array.map((map) => x * 2);
+console.log(soma);
